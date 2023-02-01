130598a35fd676ee08ea89e40faf5d8e7e178f36
praveen-x <soysuperhuman@gmail.com>
2023-02-01 23:59:25 +0530

    test module draft

--- src/main.rs
+++ src/main.rs
@@ -18,3 +18,14 @@ fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
 }
+
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
